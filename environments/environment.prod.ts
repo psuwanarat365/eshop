8b6ec07b03e1dca562b8e30196a1e87d06ec8f06
psuwanarat365 <psuwanarat365@gmail.com>
2023-02-14 00:37:19 +0700

    change to production build

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiURL: 'http://localhost:3000/api/v1/'
+    apiURL: 'https://eshop-guppy-backend.herokuapp.com/api/v1/'
 };
 //will be replace with Heroku server
